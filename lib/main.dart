213f3bc1f3eb8d320f5dd992d5358de18ae59941
SneakyNieky <niekdommelen37@gmail.com>
2024-01-23 15:04:33 +0100

    added lots of comments and some formatting changes

--- lib/main.dart
+++ lib/main.dart
@@ -32,49 +32,59 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPage extends State<ChatPage> {
+  // create list to store the widgets from chat messages
   List<Widget> elementList = [];
-
+  // create list to store received message id's
   List<dynamic> msgIdList = [];
-
+  // create TextEditingController variable for the text field
   final TextEditingController _controller = TextEditingController();
-
+  // create FocusNode variable for the text field
   FocusNode msgInputFocusNode = FocusNode();
-
+  // create variables for the server ip address and port number
   static get ipAddress => "10.59.138.103";
   static get portNumber => 3000;
-
+  // create WebSocketChannel variable and set it to null
   var channel;
 
   void _startChat() {
+    // Connect to the server
     channel = WebSocketChannel.connect(
       Uri.parse('ws://$ipAddress:$portNumber'),
     );
     setState(() {
+      // Add a system message to the list
       elementList.add(_addSystemMsg("Connected to server"));
     });
   }
 
   void _closeChat() {
     if (channel != null) {
+      // close connection with the server
       channel.sink.close();
     }
     setState(() {
+      // clear id list and set channel(connection status) to null
       channel = null;
       msgIdList.clear();
     });
   }
-
+  // create ScrollController
   final ScrollController _scrollController = ScrollController();
 
   void _scrollDown() {
+    // scroll to the bottom of the list
     _scrollController.animateTo(
+      // get the total height of the list
       _scrollController.position.extentTotal,
+      // set the duration of the animation
       duration: const Duration(seconds: 1),
+      // set the curve of the animation
       curve: Curves.fastOutSlowIn,
     );
   }
 
   Widget _addMenu() {
+    // return FractionallySizedBox with the menu
     return FractionallySizedBox(
       widthFactor: 1,
       alignment: Alignment.topCenter,
@@ -110,6 +120,7 @@ class _ChatPage extends State<ChatPage> {
   }
 
   Widget _recieveChatMsg(String msg) {
+    // return FractionallySizedBox with the received chat message
     return FractionallySizedBox(
       widthFactor: 0.8,
       alignment: Alignment.centerLeft,
@@ -133,6 +144,7 @@ class _ChatPage extends State<ChatPage> {
   }
 
   Widget _addChatMsg(String msg) {
+    // return FractionallySizedBox with the chat message
     return FractionallySizedBox(
       widthFactor: 0.8,
       alignment: Alignment.centerRight,
@@ -156,6 +168,7 @@ class _ChatPage extends State<ChatPage> {
   }
 
   Widget _addSystemMsg(String msg) {
+    // return FractionallySizedBox with the system message
     return FractionallySizedBox(
       widthFactor: 0.9,
       alignment: Alignment.center,
@@ -177,6 +190,7 @@ class _ChatPage extends State<ChatPage> {
   @override
   void initState() {
     super.initState();
+    // add menu to the list on start
     elementList.add(_addMenu());
   }
 
@@ -186,7 +200,7 @@ class _ChatPage extends State<ChatPage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Chat App",
+        title: Text("Whisper",
             style: TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
       ),
       body: Center(
@@ -204,45 +218,56 @@ class _ChatPage extends State<ChatPage> {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
+                        // Check if the connection is open
                         if (channel != null)
                           StreamBuilder(
                             stream: channel.stream,
                             builder: (context, snapshot) {
+                              // Check if the snapshot has data
                               if (snapshot.hasData) {
+                                // Decode the json data
                                 final data =
                                     jsonDecode(snapshot.data.toString())
                                         as Map<String, dynamic>;
-                                final msgId = data[
-                                    "id"]; // Replace this with your logic for generating a unique identifier
-                                final receivedMsg = data["msg"];
+                                final msgId = data["id"]; // Message id
+                                final receivedMsg = data["msg"]; // Message data (text)
                                 // Check if a message with the same identifier is not already in the list
-                                if (!msgIdList
-                                    .any((element) => element == msgId)) {
+                                if (!msgIdList.any((element) => element == msgId)) {
+                                  // Add the message id to the list
                                   msgIdList.add(msgId);
+                                  // Check if the message is a system message
                                   if (msgId.toString().contains("system_msg")) {
+                                    // Add the system message to the list
                                     elementList.add(_addSystemMsg(receivedMsg));
+                                    // Check if the message is a disconnect message
                                     if (msgId
                                         .toString()
                                         .contains("system_msg_4")) {
+                                      // close connection with the server
                                       channel = null;
+                                      // clear the list of received message id's
                                       msgIdList.clear();
+                                      // add menu to the list
                                       elementList.add(_addMenu());
                                     }
                                   } else {
+                                    // Add the message to the list
                                     elementList
                                         .add(_recieveChatMsg(receivedMsg));
                                   }
 
                                   Future.microtask(() {
+                                    // Check if the widget is mounted
                                     if (mounted) {
                                       setState(() {
+                                        // scroll to the bottom of the list
                                         _scrollDown();
                                       }); // Notify Flutter to rebuild the widget tree
                                     }
                                   });
                                 }
                               }
-                              return SizedBox.shrink();
+                              return const SizedBox.shrink();
                             },
                           )
                       ],
@@ -252,6 +277,7 @@ class _ChatPage extends State<ChatPage> {
                         controller: _scrollController,
                         itemCount: elementList.length,
                         itemBuilder: (context, index) {
+                          // Return the widget's from the list
                           return elementList[index];
                         },
                       ),
@@ -274,10 +300,15 @@ class _ChatPage extends State<ChatPage> {
                       icon: const Icon(Icons.close, color: Colors.red),
                       onPressed: () {
                         setState(() {
+                          // close connection with the server
                           _closeChat();
+                          // clear id list and set channel(connection status) to null
                           channel = null;
+                          // clear the list of received message id's
                           msgIdList.clear();
+                          // add menu to the list
                           elementList.add(_addMenu());
+                          // scroll to the bottom of the list
                           _scrollDown();
                         });
                       },
@@ -295,11 +326,17 @@ class _ChatPage extends State<ChatPage> {
                             controller: _controller,
                             onSubmitted: (String value) {
                               setState(() {
+                                // Check if the message is empty
                                 if (value.trim().isEmpty) return;
+                                // Add the message to the list
                                 elementList.add(_addChatMsg(value));
+                                // empty the controller and text field
                                 _controller.clear();
+                                // set focus to the text field
                                 msgInputFocusNode.requestFocus();
+                                // scroll to the bottom of the list
                                 _scrollDown();
+                                // Send the message to the server
                                 channel.sink.add(value);
                               });
                             },
@@ -325,11 +362,17 @@ class _ChatPage extends State<ChatPage> {
                       icon: const Icon(Icons.send, color: Colors.white),
                       onPressed: () {
                         setState(() {
+                          // Check if the message is empty
                           if (_controller.text.trim().isEmpty) return;
+                          // Add the message to the list
                           elementList.add(_addChatMsg(_controller.text));
+                          // Send the message to the server
                           channel.sink.add(_controller.text);
+                          // empty the controller and text field
                           _controller.clear();
+                          // set focus to the text field
                           msgInputFocusNode.requestFocus();
+                          // scroll to the bottom of the list
                           _scrollDown();
                         });
                       },
