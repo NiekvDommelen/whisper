7cc6e0b853915b9f4ae558f40ceb22ac1e156dac
SneakyNieky <niekdommelen37@gmail.com>
2024-01-23 14:52:32 +0100

    lots of changes regarding communication between app and server

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() {
@@ -20,30 +20,13 @@ class MyApp extends StatelessWidget {
       ),
 
       routes:{
-        '/': (context) => const HomePage(),
-        '/chat': (context) => const ChatPage(),
+        '/': (context) => const ChatPage(),
       }
 
     );
   }
 }
 
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePage();
-}
-
-class _HomePage extends State<HomePage>{
-  @override
-  Widget build(BuildContext context) {
-
-
-  }
-
-}
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -66,12 +49,29 @@ class _ChatPage extends State<ChatPage> {
   FocusNode msgInputFocusNode = FocusNode();
 
 
-  static get ipAddress => "10.59.138.115";
+  static get ipAddress => "10.59.138.103";
   static get portNumber => 3000;
 
-  final channel = WebSocketChannel.connect(
-    Uri.parse('ws://$ipAddress:$portNumber'),
-  );
+  var  channel;
+
+  void _startChat(){
+    channel = WebSocketChannel.connect(
+      Uri.parse('ws://$ipAddress:$portNumber'),
+    );
+    setState(() {
+      elementList.add(_addSystemMsg("Connected to server"));
+    });
+  }
+
+  void _closeChat() {
+    if (channel != null) {
+      channel.sink.close();
+    }
+    setState(() {
+      channel = null;
+      msgIdList.clear();
+    });
+  }
 
   final ScrollController _scrollController = ScrollController();
 
@@ -83,6 +83,42 @@ class _ChatPage extends State<ChatPage> {
       curve: Curves.fastOutSlowIn,
     );
   }
+
+  Widget _addMenu(){
+    return FractionallySizedBox(
+      widthFactor: 1,
+      alignment: Alignment.topCenter,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children:[Container(
+            constraints: const BoxConstraints(minWidth: 20, maxWidth: double.infinity),
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(7),
+
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text("Welcome to Whisper!!", style: TextStyle(color: Colors.white)),
+                  const SizedBox(height: 10),
+                  TextButton(
+                    style: TextButton.styleFrom(
+
+                      backgroundColor: Colors.deepPurple,
+
+                    ),
+                    onPressed: () {
+                      elementList.clear();
+                      _startChat();
+                    },
+                    child: const Text("Start Chat", style: TextStyle(color: Colors.white)),
+                  ),
+                ]
+            )
+        )],
+      ),
+    );
+  }
+
 
 
   Widget _recieveChatMsg(String msg) {
@@ -127,12 +163,35 @@ class _ChatPage extends State<ChatPage> {
 
   }
 
+  Widget _addSystemMsg(String msg) {
+    return FractionallySizedBox(
+      widthFactor: 0.9,
+      alignment: Alignment.center,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+
+        children:[Container(
+          constraints: BoxConstraints(minWidth: 20, maxWidth: double.infinity),
+          margin: const EdgeInsets.all(10),
+          padding: const EdgeInsets.all(7),
+
+          child: Text(msg, style: const TextStyle(color: Colors.white)),
+        )],
+      ),
+    );
+
+  }
+
 
 
   @override
+  void initState() {
+    super.initState();
+    elementList.add(_addMenu());
+  }
+  @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
 
@@ -146,10 +205,9 @@ class _ChatPage extends State<ChatPage> {
 
           children: [
 
-            SizedBox(
+            Expanded(
               key: const ValueKey("chat_input"),
-              width: screenWidth,
-              height: screenHeight - 116,
+
               child: Container(
                 decoration: const BoxDecoration(
                   color: Color.fromRGBO(69, 69, 69, 1.0),
@@ -160,18 +218,10 @@ class _ChatPage extends State<ChatPage> {
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children:[Container(
-                        constraints: BoxConstraints(minWidth: 100, maxWidth: screenWidth * 0.8),
-                        margin: const EdgeInsets.all(10),
-                        padding: const EdgeInsets.all(7),
-                        decoration: BoxDecoration(
-                          color: Colors.cyan,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Text("Filler Filler Filler Filler Filler Filler Filler Filler Filler Filler Filler ", style: TextStyle(color: Colors.white)),
-                      ),
-                        StreamBuilder(
-                          stream: channel.stream,
+                      children:[
+                        if(channel != null) StreamBuilder(
+
+                          stream: channel.stream ,
                           builder: (context, snapshot) {
 
                             if (snapshot.hasData) {
@@ -181,7 +231,16 @@ class _ChatPage extends State<ChatPage> {
                               // Check if a message with the same identifier is not already in the list
                               if (!msgIdList.any((element) => element == msgId)) {
                                 msgIdList.add(msgId);
-                                elementList.add(_recieveChatMsg(receivedMsg));
+                                if(msgId.toString().contains("system_msg")){
+                                  elementList.add(_addSystemMsg(receivedMsg));
+                                  if(msgId.toString().contains("system_msg_4")){
+                                    channel = null;
+                                    msgIdList.clear();
+                                    elementList.add(_addMenu());
+                                  }
+                                }else{
+                                  elementList.add(_recieveChatMsg(receivedMsg));
+                                }
 
                                 Future.microtask(() {
                                   if (mounted) {
@@ -194,20 +253,20 @@ class _ChatPage extends State<ChatPage> {
                             return SizedBox.shrink();
 
                           },
-                        ),
+                        )
                       ],
 
                     ),
-                    Expanded(
+                        Expanded(
 
-                      child: ListView.builder(
-                        controller: _scrollController,
-                        itemCount: elementList.length,
-                        itemBuilder: (context, index) {
-                          return elementList[index];
-                        },
-                      ),
-                    ),
+                          child: ListView.builder(
+                            controller: _scrollController,
+                            itemCount: elementList.length,
+                            itemBuilder: (context, index) {
+                              return elementList[index];
+                            },
+                          ),
+                        ),
 
 
 
@@ -229,79 +288,91 @@ class _ChatPage extends State<ChatPage> {
 
                   ),
                   child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      SizedBox(
-                          width: screenWidth * 0.8,
-                          height: 50,
-                          child: Container(
-                            margin: const EdgeInsets.all(2),
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.close, color: Colors.red),
+                          onPressed: () {
+                            setState(() {
+                              _closeChat();
+                              channel = null;
+                              msgIdList.clear();
+                              elementList.add(_addMenu());
+                              _scrollDown();
+                            });
+                          },
+                        ),
+                        SizedBox(
+                            width: screenWidth * 0.7,
+                            height: 50,
+                            child: Container(
+                              margin: const EdgeInsets.all(2),
 
-                            child: TextField(
-
-
-                              minLines: 1,
-                              maxLines: 1,
-                              obscureText: false,
-                              focusNode: msgInputFocusNode,
-                              controller: _controller,
-                              onSubmitted: (String value) {
-                                setState(() {
-                                  if(value.trim().isEmpty) return;
-                                  elementList.add(_addChatMsg(value));
-                                  _controller.clear();
-                                  msgInputFocusNode.requestFocus();
-                                  _scrollDown();
-                                  channel.sink.add(value);
+                              child: TextField(
 
 
+                                minLines: 1,
+                                maxLines: 1,
+                                obscureText: false,
+                                focusNode: msgInputFocusNode,
+                                controller: _controller,
+                                onSubmitted: (String value) {
+                                  setState(() {
+                                    if(value.trim().isEmpty) return;
+                                    elementList.add(_addChatMsg(value));
+                                    _controller.clear();
+                                    msgInputFocusNode.requestFocus();
+                                    _scrollDown();
+                                    channel.sink.add(value);
 
-                                });
-                              },
-                              textAlignVertical: TextAlignVertical.center,
-                              style: const TextStyle(color: Colors.white),
-                              cursorColor: Colors.white12,
-
-                              decoration: const InputDecoration(
-                                isDense: true,
 
 
-                                focusColor: Colors.white,
-                                hoverColor: Colors.white,
+                                  });
+                                },
+                                textAlignVertical: TextAlignVertical.center,
+                                style: const TextStyle(color: Colors.white),
+                                cursorColor: Colors.white12,
 
-                                enabledBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.grey),
+                                decoration: const InputDecoration(
+                                  isDense: true,
 
+
+                                  focusColor: Colors.white,
+                                  hoverColor: Colors.white,
+
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.grey),
+
+                                  ),
+
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                  ),
+
+                                  hintText: 'Message',
+                                  hintStyle: TextStyle(color: Colors.grey),
                                 ),
-
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white),
-                                ),
-
-                                hintText: 'Message',
-                                hintStyle: TextStyle(color: Colors.grey),
                               ),
-                            ),
-                          )
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.send, color: Colors.white),
-                        onPressed: () {
-                          setState(() {
-                            if(_controller.text.trim().isEmpty) return;
-                            elementList.add(_addChatMsg(_controller.text));
-                            channel.sink.add(_controller.text);
-                            _controller.clear();
-                            msgInputFocusNode.requestFocus();
-                            _scrollDown();
+                            )
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.send, color: Colors.white),
+                          onPressed: () {
+                            setState(() {
+                              if(_controller.text.trim().isEmpty) return;
+                              elementList.add(_addChatMsg(_controller.text));
+                              channel.sink.add(_controller.text);
+                              _controller.clear();
+                              msgInputFocusNode.requestFocus();
+                              _scrollDown();
 
-                          });
-                        },
-                      ),
-                    ],
-                  ),
-                )
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  )
               ],
             )
           ],
