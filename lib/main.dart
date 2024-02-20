50f07fe296fabcfbe471727fdf7076d66219b9b9
SneakyNieky <niekdommelen37@gmail.com>
2024-02-20 13:27:39 +0100

    route changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 
 import 'screens/homepage.dart';
 import 'screens/login.dart';
+import 'screens/signup.dart';
 import 'screens/chatpage.dart';
 
 void main() {
@@ -12,24 +13,37 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // Define a boolean flag to track user authentication status
+  // return true if user is authenticated, else return false
+  bool checkUserAuthenticationStatus() {
+    return false;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Whisper',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-              primary: const Color.fromARGB(255, 81, 43, 129),
-              onPrimary: const Color.fromARGB(255, 68, 119, 206),
-              secondary: const Color.fromARGB(255, 53, 21, 93),
-              onSecondary: const Color.fromARGB(40, 84, 35, 146),
-              surface: const Color.fromARGB(255, 53, 21, 93),
-              brightness: Brightness.dark,
-              seedColor: const Color.fromARGB(255, 53, 21, 93)),
+      title: 'Whisper',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          primary: const Color.fromARGB(255, 81, 43, 129),
+          onPrimary: const Color.fromARGB(255, 68, 119, 206),
+          secondary: const Color.fromARGB(255, 53, 21, 93),
+          onSecondary: const Color.fromARGB(40, 84, 35, 146),
+          surface: const Color.fromARGB(255, 53, 21, 93),
+          brightness: Brightness.dark,
+          seedColor: const Color.fromARGB(255, 53, 21, 93),
         ),
-        routes: {
-          '/': (context) => const LoginPage(), // Homepage()
-        });
+      ),
+      // Check user authentication status and route accordingly
+      initialRoute: checkUserAuthenticationStatus() ? '/' : '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/signup': (context) => const SignupPage(),
+        '/': (context) => const HomePage(),
+        '/chat': (context) => const ChatPage(),
+      },
+    );
   }
 }
