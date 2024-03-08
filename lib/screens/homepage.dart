609e1351bdf499b79583dfee15bcc68e0e6a3ec4
SneakyNieky <niekdommelen37@gmail.com>
2024-03-08 13:39:09 +0100

    imported Shared_preferences

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
