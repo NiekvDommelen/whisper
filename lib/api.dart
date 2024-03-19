0d4447aae425c490dd9961632a7c3c400f1b9de0
SneakyNieky <niekdommelen37@gmail.com>
2024-03-19 09:32:24 +0100

    added debug page

--- lib/api.dart
+++ lib/api.dart
@@ -1,3 +1,85 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'package:whisper/userdata.dart';
+
+class api{
+  static const String _baseURL = "http://10.59.138.23:3000/api/";
+
+
+  api();
+  // ____DEBUG____
+  Future<dynamic> debugApiData() async {
+    var userdata = await getUserData();
+    var data = {"_baseURL": _baseURL, "Userid": userdata["id"], "Username": userdata["username"], "Email": userdata["email"]};
+    return data;
+  }
+  // ____END DEBUG____
+
+  // void setUserData() async {
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   debugPrint('Logged in: ${prefs.getBool('loggedIn') ?? false}');
+  //   if(prefs.getBool('loggedIn') ?? false == false){
+  //     return;
+  //   }
+  //   var data = await getUserData();
+  //   Userdata.userid = data["id"];
+  //   Userdata.username = data["username"];
+  //   Userdata.email = data["email"];
+  //   userdata(Userdata.userid, Userdata.username, Userdata.email);
+  // }
+
+  Future<int> getUserId() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final userid = prefs.getInt("userid") ?? 0;
+    return userid;
+  }
+
+  Future<dynamic> searchUserData(userid) async {
+    String address = "${_baseURL}user";
+
+    var response = await http.post(Uri.parse(address),
+
+      headers: <String, String>{
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      },
+      body: {
+        'userid': userid.toString(),
+      },
+    );
+
+    if(response.body.isNotEmpty) {
+      var $data = jsonDecode(response.body);
+      return $data;
+    }else{
+      return null;
+    }
+  }
+
+  Future<dynamic> getUserData() async {
+    int userid = await getUserId();
+    String address = "${_baseURL}user";
+
+    var response = await http.post(Uri.parse(address),
+
+      headers: <String, String>{
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      },
+      body: {
+        'userid': userid.toString(),
+      },
+    );
+
+    if(response.body.isNotEmpty) {
+      var $data = jsonDecode(response.body);
+      return $data;
+    }else{
+      return null;
+    }
+
+  }
+
   Future<bool> loginUser(String username, String password) async{
     String address = "${_baseURL}login";
 
@@ -21,6 +103,7 @@
       return false;
     }
   }
+
   Future<bool> signupUser(String username, String email, String password) async {
     String address = "${_baseURL}signup";
     var response = await http.post(Uri.parse(address),
