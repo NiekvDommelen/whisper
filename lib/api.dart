b15c81998d73c428bec72151e617a3e7da7ad86d
SneakyNieky <niekdommelen37@gmail.com>
2024-03-19 09:31:58 +0100

    moved api communication from login.dart to api.dart

--- lib/api.dart
+++ lib/api.dart
@@ -1,3 +1,26 @@
+  Future<bool> loginUser(String username, String password) async{
+    String address = "${_baseURL}login";
+
+    var response = await http.post(Uri.parse(address),
+
+      headers: <String, String>{
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      },
+      body: {
+        'username': username,
+        'password': password,
+      },
+    );
+    var $data = jsonDecode(response.body);
+    if($data["success"] == true){
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setBool('loggedIn', true);
+      prefs.setInt('userid', $data["userid"]);
+      return true;
+    }else{
+      return false;
+    }
+  }
   Future<bool> signupUser(String username, String email, String password) async {
     String address = "${_baseURL}signup";
     var response = await http.post(Uri.parse(address),
